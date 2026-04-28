fdbbf8d4a73eb2346abf029d14a1c7865a09b1d7
Cristian_Martinez <cfmartinezs@hotmail.com>
2026-04-27 22:46:03 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -182,7 +182,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/123103_CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
